a3287ccab04fd6139be7ae141d4c4700dad41eae
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-05-23 21:53:37 +0700

    added data viz portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -16,20 +16,12 @@ const Portfolio = () => (
         <div style={{ padding: `1em`}}>
           <dl>
             <dt>
-              <a style={{color: `#1A090D`, fontWeight: `600`}} href="https://github.com/ParadiseCatz/IF2110-Tubes-Alstruk--Monopoly">
-                Monopoly Simulation (2015)
+              <a style={{color: `#1A090D`, fontWeight: `600`}}  href="https://github.com/teaolivia/Pamitran">
+                Pamitran Electric e-Shop (start 2018)
               </a>
             </dt> 
-            <dd>A game board up to 4 players similar to Monopoly.Create by <span style={{color: `#083D77`}}><b>C++.</b></span></dd>
+            <dd>a web-based hometown-coverage mini e-commerce. <span style={{color: `#083D77`}}><b>Python. Google Cloud Functions. Django. React.js.</b></span></dd>  
 
-            <dt> 
-              <a style={{color: `#1A090D`, fontWeight: `600`}} href="https://github.com/teaolivia/SmartATK">
-                SmartATK (2016)
-              </a>
-            </dt> 
-            <dd>A web-based application for managing office stationary inventory in a
-            university. <span style={{color: `#083D77`}}><b>PHP. Laravel. SQL.</b></span></dd> 
-            
             <dt>
               <a style={{color: `#1A090D`, fontWeight: `600`}}  href="https://github.com/keychera/AquaAesthetic">
                 AquaAesthetic (2017)
@@ -38,12 +30,28 @@ const Portfolio = () => (
             <dd>create an aquarium simulation with user can interact by feeding and buying
               fishes. <span style={{color: `#083D77`}}><b>Java. Java Swing. JUnit.</b></span></dd> 
             
-            <dt>
-              <a style={{color: `#1A090D`, fontWeight: `600`}}  href="https://github.com/teaolivia/Pamitran">
-                Pamitran Electric e-Shop (start 2018)
+              <dt>
+              <a style={{color: `#1A090D`, fontWeight: `600`}} href="https://visdat-cihuy.github.io">
+                Unemployment Rate and Indonesian Presidential Term Correlation Viz (2017)
               </a>
             </dt> 
-            <dd>a web-based hometown-coverage mini e-commerce. <span style={{color: `#083D77`}}><b>Python. Google Cloud Functions. Django. React.js.</b></span></dd>  
+            <dd>a data visualization for Indonesian unemployment rate in each presidency. <span style={{color: `#083D77`}}><b>JavaScript. Bootstrap. Leaf.js. HTML. CSS.</b></span></dd>
+
+            <dt> 
+              <a style={{color: `#1A090D`, fontWeight: `600`}} href="https://github.com/teaolivia/SmartATK">
+                SmartATK (2016)
+              </a>
+            </dt> 
+            <dd>A web-based application for managing office stationary inventory in a
+            university. <span style={{color: `#083D77`}}><b>PHP. Laravel. SQL.</b></span></dd> 
+
+            <dt>
+              <a style={{color: `#1A090D`, fontWeight: `600`}} href="https://github.com/ParadiseCatz/IF2110-Tubes-Alstruk--Monopoly">
+                Monopoly Simulation (2015)
+              </a>
+            </dt> 
+            <dd>A game board up to 4 players similar to Monopoly.Create by <span style={{color: `#083D77`}}><b>C++.</b></span></dd>
+
           </dl>
         </div>
       </div>
